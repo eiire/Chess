ae523e63c28b2956a029fff5e6f83d87390472b3
Dmitriy Bulakh <bulax.d@mail.ru>
2018-03-29 17:20:33 +0700

    Новая реализация шахмат, через массив структур. Выделение памяти под массив структур.

--- board.c
+++ board.c
@@ -1,24 +1,8 @@
-#include <stdio.h>
+#include "board.h"
 
 int main()
 {
-	char board[8][8] = 
-    {
-		{'r','n','b','q','k','b', 'n','r'},
-        {'p','p','p','p','p','p','p','p'},
-        {' ',' ',' ',' ',' ',' ',' ',' '},
-        {' ',' ',' ',' ',' ',' ',' ',' '},
-        {' ',' ',' ',' ',' ',' ',' ',' '},
-        {' ',' ',' ',' ',' ',' ',' ',' '},
-        {'P','P','P','P','P','P','P','P'},
-        {'R','N','B','Q','K','B','N','R'},
-    };
+    Board *array_chess = (Board *)malloc(sizeof(Board)*8*8);
 
-    int i, j;
-    for (i = 0; i < 8; i++) {
-    	for (j = 0; j < 8; j++) {
-    		printf("%c ", board[i][j]);
-    	}
-        printf("\n");
-    }
+    return 0;
 }
