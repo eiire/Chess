428484354a2bc60c101c6b0f058c7da8c2c26e62
Dmitriy Bulakh <bulax.d@mail.ru>
2018-04-03 16:41:21 +0700

    Добавление include goard и скрытие .o-файлов.

--- board.h
+++ board.h
@@ -1,3 +1,6 @@
+#ifndef  BOARD_H
+#define  BOARD_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,3 +13,5 @@ void output (Board *mass);
 void makemove (Board *mass);
 
 int decode (char letter, int number);
+
+#endif
